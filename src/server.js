605dad529feed2f257fc8b4321b6300ebe3c5e80
root <root@atakan.cloud>
2024-08-01 17:03:28 +0000

    finished core and extension 1

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ const cors = require("cors");
 
 //Create a new express application
 const app = express();
+app.use(express.static("public"));
 
 //Tell express we want to use the morgan library
 app.use(morgan("dev"));
@@ -14,6 +15,42 @@ app.use(morgan("dev"));
 app.use(cors());
 //Tell express to parse JSON in the request body
 app.use(express.json());
+
+const data = { counter: 0 };
+
+app.get("/counter", function (req, res) {
+  res.json(data);
+});
+
+app.delete("/counter", function (req, res) {
+  data.counter = 0;
+  res.json(data);
+});
+
+app.post("/counter/increment", function (req, res) {
+  data.counter++;
+  res.json(data);
+});
+app.post("/counter/decrement", function (req, res) {
+  data.counter--;
+  res.json(data);
+});
+
+app.post("/counter/double", function (req, res) {
+  data.counter = data.counter * 2;
+  res.json(data);
+});
+
+app.put("/counter", function (req, res) {
+  const value = Number(req.query.value);
+  console.log(value);
+  if (!isNaN(value)) {
+    data.counter = value;
+    res.json(data);
+  } else {
+    res.status(418).json("invalid");
+  }
+});
 
 //Export our app so other files can run it
 module.exports = app;
