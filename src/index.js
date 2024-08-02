d48ecf06dba693ab148975ebdb609bee3f18c512
Atakan Temizkan <atikotemizkan@gmail.com>
2024-08-02 07:25:14 +0000

    added SSL and commented it

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
 const app = require("./server.js");
 const port = 3030;
 
+// const https = require("https");
+// const fs = require("fs");
+//
+// var privateKey = fs.readFileSync(
+// "/etc/letsencrypt/live/atakan.cloud/privkey.pem"
+// );
+// var certificate = fs.readFileSync(
+// "/etc/letsencrypt/live/atakan.cloud/fullchain.pem"
+// );
+//
+// https
+// .createServer(
+// {
+// key: privateKey,
+// cert: certificate,
+// },
+// app
+// )
+// .listen(port, () => {
+// console.log(`Server is running on https://atakan.cloud:${port}/`);
+// });
+
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}/`);
+  console.log(`Server is running on http://atakan.cloud:${port}/`);
 });
